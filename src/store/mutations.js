4588b148658716af8c8c620133d76afd6bc2d3ec
dingli123 <601220272@qq.com>
2017-09-05 11:58:54 +0800

    scroll

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -4,13 +4,8 @@ const mutations = {
   [types.SET_CITY](state,city) {
     state.city = city
   },
-  [types.SET_FOODS](state, FOODS) {
-    state.foods = FOODS
   [types.SET_GOODS](state, goods) {
     state.goods = goods
-  },
-  [types.GET_SHOPLIST_INDEX](state, SHOPLIST) {
-    state.shoplist = SHOPLIST
   }
 };
 
