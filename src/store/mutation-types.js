88d61acb2d7c8af59dd06d3d0a962ee0de0f3018
xie <1111@qq.com>
2017-09-05 12:02:42 +0800

    sds

--- src/store/mutation-types.js
+++ src/store/mutation-types.js
@@ -1,3 +1,3 @@
 export const SET_CITY = 'SET_CITY';
 export const SET_GOODS = 'SET_GOODS';
-
+export const GET_SHOPLIST_INDEX = 'GET_SHOPLIST_INDEX'
--- src/store/mutations.js
+++ src/store/mutations.js
@@ -6,6 +6,9 @@ const mutations = {
   },
   [types.SET_GOODS](state, goods) {
     state.goods = goods
+  },
+  [types.GET_SHOPLIST_INDEX](state, SHOPLIST) {
+    state.shoplist = SHOPLIST
   }
 };
 
